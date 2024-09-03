7b9798360d85f3a5854a80ace098b9b868922965
Tusho7 <sandrotushurashvili7@gmail.com>
2024-09-03 15:02:53 +0400

    feat: connect to the database

--- src/server.js
+++ src/server.js
@@ -1,10 +1,32 @@
 import bodyParser from "body-parser";
 import express from "express";
+import sequelize from "./config/database.js";
+import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 
 app.use(bodyParser.json());
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const corsOptions = {
+  origin: true,
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
