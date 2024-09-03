dce25cd554c43e2fd1ff1c18758a917996850837
Tusho7 <sandrotushurashvili7@gmail.com>
2024-09-03 14:43:10 +0400

    initialize project

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
