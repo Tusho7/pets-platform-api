6140a7147a7908fee11cdc390a9fa6433deed939
Tusho7 <sandrotushurashvili7@gmail.com>
2024-09-04 09:27:53 +0400

    feat: create user login and register functionality

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const createUser = async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
@@ -16,19 +17,80 @@ export const createUser = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    const profilePicturePath = file
+      ? "profil-pictures/" + file.filename
+      : "profil-pictures/default.png";
+
     const newUser = new User({
       email,
       password: hashedPassword,
       firstName,
       lastName,
-      profilePicture: "profil-pictures/" + file.originalname,
+      profilePicture: profilePicturePath,
     });
 
     await newUser.save();
 
-    res.status(201).json({ message: "მომხმარებელი შეიქმნა წარმატებით" });
+    res.status(201).json({ message: "ყველა ველი აუცილებელია." });
   } catch (error) {
-    console.error("dafiqsirda Secdoma:", error);
+    console.error("დაფიქსირდა შეცდომა:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+    const userId = req.user?.id;
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error getting user:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password)
+      return res.status(400).json({ message: "ყველა ველი აუცილებელია." });
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: "მომხმარემებელი ვერ მოიძებნა." });
+    }
+    if (user.isBlocked)
+      return res
+        .status(401)
+        .json({ message: "თქვენი მომხმარებელი დაბლოკილია." });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { email: user.email, id: user.id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 3600000,
+      secure: true,
+    });
+
+    res.status(200).json({ id: user.id });
+  } catch (error) {
+    console.error("Error logging in user:", error);
     res.status(500).json({ message: "Server error" });
   }
 };
--- src/routes/User.js
+++ src/routes/User.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as userController from "../controllers/User.js";
 import { fileStorage, fileFilter } from "../utils/multer.js";
 import multer from "multer";
+import { middleware } from "../middlewares/auth.js";
 
 const router = express.Router();
 
@@ -10,5 +11,9 @@ router.post(
   multer({ storage: fileStorage, fileFilter }).single("profil-picture"),
   userController.createUser
 );
+
+router.post("/login", userController.loginUser);
+
+router.get("/get_user", middleware, userController.getUser);
 
 export default router;
