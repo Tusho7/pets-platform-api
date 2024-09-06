23ac53758c875d69d53ce90650735a582386ca57
Tusho7 <sandrotushurashvili7@gmail.com>
2024-09-06 12:04:42 +0400

    feat: remove create-faq route from Faq.js

--- src/routes/Faq.js
+++ src/routes/Faq.js
@@ -3,7 +3,6 @@ import * as faqController from "../controllers/Faq.js";
 
 const router = express.Router();
 
-router.post("/create-faq", faqController.createFaq);
 router.get("/faqs", faqController.getFaqs);
 
 export default router;
